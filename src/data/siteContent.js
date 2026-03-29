572b8c0c78a35e40d5b586d93cc9eca421e0ab97
Joao Pedro <joaopedro.jpmaia@gmail.com>
2026-03-29 15:21:01 -0300

    fix whatsapp definitivo

--- src/data/siteContent.js
+++ src/data/siteContent.js
@@ -3,7 +3,7 @@ export const officeInfo = {
   shortName: 'Fernando Luvizotto',
   oab: 'OAB/SP 399821',
   city: 'Piracicaba/SP',
-  whatsappNumber: '551998128-6842',
+  whatsappNumber: '5519981286842',
   whatsappDisplay: '(19) 98128-6842',
   email: 'contato@escritorio.com.br',
   addressLabel: 'Piracicaba/SP',
